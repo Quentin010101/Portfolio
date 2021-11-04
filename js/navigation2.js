782d17feebf0bac2aa8d39a7643cd177ec52f559
Quentin <q_cozic@hotmail.fr>
2021-11-04 22:22:04 +0100

    l

--- js/navigation2.js
+++ js/navigation2.js
@@ -8,11 +8,18 @@ const l2 = document.querySelector('nav li:nth-child(2) a')
 const l3 = document.querySelector('nav li:nth-child(3) a')
 const l4 = document.querySelector('nav li:nth-child(4) a')
 
+let scrollExplication = document.querySelector('footer h6')
+let burgerIcon = document.querySelector('.burger')
+let backgroundNav = document.querySelector('nav ul')
+
 let arrL = [l1, l2, l3, l4]
 let arrNav
 
+//---------- Mise a jour couleur onload ------------------------
 checkEverySectionPosition()
 miseAJourNav(checkPosition())
+miseAJourColor(checkPosition(), burgerIcon)
+miseAJourColor(checkPosition(), backgroundNav)
 // ------------ Navigation on scroll ------------------------------------------
 function checkDeltaY(parm) {                                                                       //----------- Vérifie si le scroll est positif ou negatif
     if (parm.deltaY > 0) {
@@ -54,9 +61,16 @@ function scrollToPosition(parm) {
     if (checkDeltaY(parm)) {
         scrollTo(0, checkNextPosition(1))
         miseAJourNav(checkNextPosition(1))
+        miseAJourColor(checkNextPosition(1), burgerIcon)
+        miseAJourColor(checkNextPosition(1), scrollExplication)
+        miseAJourColor(checkNextPosition(1), backgroundNav)
+        
     } else {
         scrollTo(0, checkNextPosition(0))
         miseAJourNav(checkNextPosition(0))
+        miseAJourColor(checkNextPosition(0), burgerIcon)
+        miseAJourColor(checkNextPosition(0), scrollExplication)
+        miseAJourColor(checkNextPosition(0), backgroundNav)
     }
 }
 document.addEventListener('wheel', function (event) {
@@ -65,12 +79,11 @@ document.addEventListener('wheel', function (event) {
 // ---------------- Navigation on click -------------------------------------
 arrL.forEach((element)=>{
     element.addEventListener('click', function(){
-        console.log(element)
         switch(element){
-            case l1: console.log('1'); changeColor(0) ; break;
-            case l2: console.log('2'); changeColor(1) ; break;
-            case l3: console.log('3'); changeColor(2) ; break;
-            case l4: console.log('4'); changeColor(3) ; break;
+            case l1: miseAJourColor(arrNav[0], backgroundNav); miseAJourColor(arrNav[0], scrollExplication); miseAJourColor(arrNav[0], burgerIcon); changeColor(0) ; break;
+            case l2: miseAJourColor(arrNav[1], backgroundNav); miseAJourColor(arrNav[1], scrollExplication); miseAJourColor(arrNav[1], burgerIcon); changeColor(1) ; break;
+            case l3: miseAJourColor(arrNav[2], backgroundNav); miseAJourColor(arrNav[2], scrollExplication); miseAJourColor(arrNav[2], burgerIcon); changeColor(2) ; break;
+            case l4: miseAJourColor(arrNav[3], backgroundNav); miseAJourColor(arrNav[3], scrollExplication); miseAJourColor(arrNav[3], burgerIcon); changeColor(3) ; break;
         }
     })
 })
@@ -104,4 +117,33 @@ function changeColor (parm){
     arrL[parm].classList.remove('navColorClasseWhite')
     arrL[parm].classList.remove('navColorClasseBlack')
     arrL[parm].classList.add('navColorClasseSpecial')
+}
+//---------Mise à jour color burger-----------------------------------------
+function miseAJourColor(position, element){
+    switch(position){
+        case arrNav[0]: elementChangeColor(0, element)  ; break;
+        case arrNav[1]: elementChangeColor(1, element)  ; break;
+        case arrNav[2]: elementChangeColor(0, element)  ; break;
+        case arrNav[3]: elementChangeColor(1, element)  ; break;
+    }
+}
+function elementChangeColor (parm, element){
+    
+    if(parm == 0){
+        if(element == backgroundNav){
+            element.classList.remove('ulBackgroundWhite')
+            element.classList.add('ulBackgroundBlack')
+        } else {
+            element.classList.remove('elementColorClasseBlack')
+            element.classList.add('elementColorClasseWhite')
+        }
+    } else {
+        if(element == backgroundNav){
+            element.classList.remove('ulBackgroundBlack')
+            element.classList.add('ulBackgroundWhite')
+        } else {
+            element.classList.remove('elementColorClasseWhite')
+            element.classList.add('elementColorClasseBlack')
+        }
+    }
 }
