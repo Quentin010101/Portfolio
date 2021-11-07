2f52b43e1be386c19b32d5907256aec81d6e9bdc
Quentin <q_cozic@hotmail.fr>
2021-11-07 11:47:38 +0100

    p

--- js/section-aPropos.js
+++ js/section-aPropos.js
@@ -2,149 +2,61 @@ const mod1 = document.querySelector('.module1')
 const mod2 = document.querySelector('.module2')
 const mod3 = document.querySelector('.module3')
 
+
 const icon = document.querySelectorAll('ion-icon')
 
-let iconIntervale
-let iconIntervale1
-
 let arrModule = [mod1,mod2,mod3]
-window.addEventListener('resize', function(){
-    
-})
-arrModule.forEach((element) =>{
-    clearTimeout(iconIntervale)
-    clearTimeout(iconIntervale1)
-    if(window.innerWidth > 800){
-        //-------Media Query screen > 800 px-------------
-        element.addEventListener('click', () =>{
-            arrModule.forEach((elem) =>{
-                if(elem == element){
-                    moveIconDown(element)
-                    moveIconLeft(element)
-                    iconIntervale = setTimeout(()=>{
-                        elementAggrandir(element)
-                        pOpacity(elem)
-                    }, 500)
-    
-                } else {
-                    iconIntervale1 = setTimeout(()=>{
-                        moveIconRight(elem)
-                        elementRetrecir(elem)
-                        pOpacityDown(elem)
-                        setTimeout(()=>{
-                            moveIconUp(elem)
-                        },500)
-                    },500)
-                }
-            })
-        }) 
 
-    } else {
-        //-------Media Query screen < 800 px-------------
-        element.addEventListener('click', () =>{
-            arrModule.forEach((elem) =>{
-                if(elem == element){
-                    moveIconLeft800(element)
-                    moveIconDown800(element)
-                    iconIntervale = setTimeout(()=>{
-                        elementAggrandir800(element)
-                        pOpacity(elem)
+arrModule.forEach((element)=>{
+    element.addEventListener('click', function(){
+        let openTime
+        clearTimeout(openTime)
+        ouvertureIcon(element)
+        openTime = setTimeout(()=>{
+            element.classList.remove('moduleFerme')
+            element.classList.add('moduleOuvertExtend')
+            let openTime2
+            openTime2 = setTimeout(()=>{
+                element.classList.remove('moduleOuvertExtend')
+                element.classList.add('moduleOuvert')
+                element.getElementsByTagName('div')[0].classList.remove('module-container-text-opacity0')
+                element.getElementsByTagName('div')[0].classList.add('module-container-text-opacity1')
+            }, 500)
+        }, 500)
+        arrModule.forEach((elem)=>{
+            if(elem !== element){
+                if(elem.classList.contains('moduleOuvert')){
+                    openTime = setTimeout(()=>{
+                        fermetureIcon(elem)
+                        elem.classList.remove('moduleOuvert')
+                        elem.classList.add('moduleFerme')
+                        elem.getElementsByTagName('div')[0].classList.remove('module-container-text-opacity1')
+                        elem.getElementsByTagName('div')[0].classList.add('module-container-text-opacity0')
                     }, 500)
-        
-                } else {
-                    iconIntervale1 = setTimeout(()=>{
-                        elementRetrecir800(elem)
-                        pOpacityDown(elem)
-                        setTimeout(()=>{
-                            moveIconRight800(elem)
-                            moveIconUp800(elem)
-                        },500)
-                    },500)
                 }
-            })
-        }) 
-    }
+            }
+        })
+    })
 })
 
-// ------------------- media query > 800px -------------------------
-function moveIconDown (parm){
-    parm.firstElementChild.style.bottom = "20px"
-    parm.firstElementChild.style.transform = "translate(-50%,0)"
-    parm.firstElementChild.style.transition = "all 0.5s"
+// -------------- Deplacement des icon ---------------
+function ouvertureIcon (parm){
+    let openTime2
+    let iconCible = parm.getElementsByTagName('ion-icon')[0]
+    iconCible.classList.remove('moduleIconFerme')
+    iconCible.classList.add('moduleIconOuvertExtend')
+    openTime2 = setTimeout(()=>{
+        iconCible.classList.remove('moduleIconOuvertExtend')
+        iconCible.classList.add('moduleIconOuvert')
+    }, 500)
 }
-function elementAggrandir(parm){
-    parm.style.width = "72%"
-    parm.style.transition = "width 0.4s"
-    setTimeout(()=>{
-        parm.style.width = "70%"
-        parm.style.transition = "width 0.1s"
-    },400)
-}
-function moveIconLeft (parm){
-    parm.firstElementChild.style.left = "20px"
-    parm.firstElementChild.style.transform = "translate(0,0)"
-    parm.firstElementChild.style.transition = "all 0.4s"
-}
-function moveIconUp(parm){
-    parm.firstElementChild.style.bottom = "50%"
-    parm.firstElementChild.style.transform = "translate(-50%,50%)"
-    parm.firstElementChild.style.transition = "all 0.5s"
-}
-function elementRetrecir(parm){
-    parm.style.width = "8%"
-    parm.style.transition = "width 0.4s"
-    setTimeout(()=>{
-        parm.style.width = "10%"
-        parm.style.transition = "width 0.1s"
-    },400)
-}
-function moveIconRight (parm){
-    parm.firstElementChild.style.left = "50%"
-    // parm.firstElementChild.style.transform = "translate(-50%,0)"
-    parm.firstElementChild.style.transition = "all 0.4s"
-}
-function pOpacity (parm){
-    parm.lastElementChild.style.opacity = "1"
-    parm.lastElementChild.style.transition = "all 0.5s"
-}
-function pOpacityDown (parm){
-    parm.lastElementChild.style.opacity = "0"
-    parm.lastElementChild.style.transition = "all 0.3s"
-}
-// ------------------- media query < 800px -------------------------
-function moveIconLeft800 (parm){
-    parm.firstElementChild.style.left = "20px"
-    parm.firstElementChild.style.transform = "translate(0,50%)"
-    parm.firstElementChild.style.transition = "all 0.5s"
-}
-function elementAggrandir800 (parm){
-    parm.style.height = "72%"
-    parm.style.transition = "height 0.4s"
-    setTimeout(()=>{
-        parm.style.height = "70%"
-        parm.style.transition = "height 0.1s"
-    },400)
-}
-function moveIconDown800 (parm){
-    parm.firstElementChild.style.bottom = "20px"
-    parm.firstElementChild.style.transform = "translate(0,0)"
-    parm.firstElementChild.style.transition = "all 0.4s"
-}
-function moveIconRight800 (parm){
-    parm.firstElementChild.style.left = "50%"
-    parm.firstElementChild.style.transform = "translate(-50%,50%)"
-    parm.firstElementChild.style.transition = "all 0.5s"
-}
-function elementRetrecir800(parm){
-    parm.style.height = "8%"
-    parm.style.transition = "height 0.4s"
-    setTimeout(()=>{
-        parm.style.height = "10%"
-        parm.style.transition = "height 0.1s"
-    },400)
-}
-function moveIconUp800 (parm){
-    parm.firstElementChild.style.bottom = "50%"
-    // parm.firstElementChild.style.transform = "translate(-50%,0)"
-    parm.firstElementChild.style.transition = "all 0.4s"
+function fermetureIcon (parm){
+    let openTime2
+    let iconCible = parm.getElementsByTagName('ion-icon')[0]
+    // iconCible.classList.add('moduleIconOuvertExtend')
+    openTime2 = setTimeout(()=>{
+        iconCible.classList.remove('moduleIconOuvert')
+        // iconCible.classList.remove('moduleIconOuvertExtend')
+        iconCible.classList.add('moduleIconFerme')
+    }, 500)
 }
---
+++ js/section-projet.js
@@ -0,0 +1,47 @@
+
+const projet1 = document.querySelector('.container-projet > div:nth-child(1)')
+const projet2 = document.querySelector('.container-projet > div:nth-child(2)')
+const projet3 = document.querySelector('.container-projet > div:nth-child(3)')
+
+const root = document.querySelector(':root')
+
+let animationTime = getComputedStyle(root).getPropertyValue("--animationProjetTime")
+console.log(animationTime)
+let time = (animationTime.substring(0,animationTime.length - 1))*1000
+
+let arrProjet = [projet1, projet2, projet3]
+
+
+
+arrProjet.forEach((element)=>{
+    element.addEventListener('click', function(){
+        if(element.classList.contains('projet-position1')){
+            arrProjet[0].classList.remove('projet-position1')
+            arrProjet[0].classList.add('projet-position3')
+            arrProjet[0].classList.add('retrecirPosition')
+            setTimeout(()=>{
+                arrProjet[2].classList.remove('retrecirPosition') 
+            }, time)
+            arrProjet[1].classList.remove('projet-position2')
+            arrProjet[1].classList.add('projet-position1')
+            arrProjet[2].classList.remove('projet-position3')
+            arrProjet[2].classList.add('projet-position2')
+            let arrStockage = arrProjet.shift() 
+            arrProjet.push(arrStockage)
+        } else if(element.classList.contains('projet-position3')){
+            arrProjet[0].classList.remove('projet-position1')
+            arrProjet[0].classList.add('projet-position2')
+            arrProjet[1].classList.remove('projet-position2')
+            arrProjet[1].classList.add('projet-position3')
+            arrProjet[2].classList.remove('projet-position3')
+            arrProjet[2].classList.add('projet-position1')
+            arrProjet[2].classList.add('retrecirPosition')
+            setTimeout(()=>{
+                arrProjet[0].classList.remove('retrecirPosition') 
+            }, time)
+            let arrStockage = arrProjet.pop() 
+            arrProjet.unshift(arrStockage)
+        }
+
+    })
+})
