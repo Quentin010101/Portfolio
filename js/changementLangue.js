388547df3c3d820efec7ac54aa08e7f0d0b24173
Quentin <q_cozic@hotmail.fr>
2021-11-08 16:32:51 +0100

    j

--- js/changementLangue.js
+++ js/changementLangue.js
@@ -14,9 +14,9 @@ function updateLangue(langue1,langue2,element){
             if( i >= latence && langue2.length > i - latence){
                 langue1 = langue1.substring(0,i - latence) + langue2.substring(i - latence, i - latence + 1) + langue1.substring(i - latence + 1)
             }
-            element.innerText = langue1
+            element.innerHTML = langue1
         }, intervale)
-        intervale += 100
+        intervale += 50
     }
     intervale = 0
 }
@@ -34,30 +34,62 @@ let a1 = "Accueil -"
 let a2 = "A propos de Moi -"
 let a3 = "Projet -"
 let a4 = "Contact -"
+let a5 = "Utiliser le scroll pour naviguer"
 
 let b1 = "Home -"
 let b2 = "About me -"
 let b3 = "Project -"
 let b4 = "Contact -"
+let b5 = "Use scroll to navigate"
 
 const e1 = document.querySelector('nav li:nth-child(1) a')
 const e2 = document.querySelector('nav li:nth-child(2) a')
 const e3 = document.querySelector('nav li:nth-child(3) a')
 const e4 = document.querySelector('nav li:nth-child(4) a')
-
-
-
+const e5 = document.querySelector('footer h6')
 
 // -------- Page Accueil ----------------
-let a5 ="Formation Developpeur Web"
+let a6 ="Formation Developpeur Web"
 
-let b5 ="Formation Web Developer"
+let b6 ="Formation Web Developer"
 
-const e5 = document.querySelector('.container-titre h4')
+const e6 = document.querySelector('.container-titre h4')
+
+// --------- Page à propos ----------------
+let a7 = "A propos de moi"
+let a8 = "En pleine reconversion professionel, j'ai choisi de me lancer dans le domaine de l'informatique. Actuellement en formation de <span>Developpeur web et web mobile</span> au centre de formation de <span>l'AFPA St Jérôme</span>, je suis à la recherche d'un stage conventioner en entreprise pour une période d'approximativement 2 mois."
+let a9 = "Compétence : <span>Hard-Skills</span>"
+let a10 = "Compétence : <span>Soft-Skills</span>"
+let a11 = "- Esprit d'équipe"
+let a12 = "- Autonome"
+let a13 = "- Sens de l'organisation"
+let a14 = "- Esprit d'analyse"
+
+let b7 = "About me"
+let b8 = "Curently in professional retraining, i choosed the IT sector. I'm recieving a training course of <span>Web and mobile Developeur</span> in the <span>l'AFPA St Jérôme</span> formation center, i'm searching for a contracted internship with a period of approximatly 2 months."
+let b9 = "Expertise : <span>Hard-Skills</span>"
+let b10 = "Expertise : <span>Soft-Skills</span>"
+let b11 = "- Team spirit"
+let b12 = "- Autonome"
+let b13 = "- Organizational skills"
+let b14 = "- Analytical mind"
+
+const e7 = document.querySelector('.module1 h4')
+const e8 = document.querySelector('.module1 p')
+const e9 = document.querySelector('.module2 h4')
+const e10 = document.querySelector('.module3 h4')
+const e11 = document.querySelector('.module3 div p:nth-child(1)')
+const e12 = document.querySelector('.module3 div p:nth-child(2)')
+const e13 = document.querySelector('.module3 div p:nth-child(3)')
+const e14 = document.querySelector('.module3 div p:nth-child(4)')
+
+//---------- Page projet ----------------
+const e15 = document.querySelector('.container-projet div:nth-child(1)')
+let a15 = e15.innerHTML
 // --------Bouton ----------------------
-let arrStockageFr = [a1,a2,a3,a4,a5]
-let arrStockageEn = [b1,b2,b3,b4,b5]
-let arrStockageElement = [e1,e2,e3,e4,e5]
+let arrStockageFr = [a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14]
+let arrStockageEn = [b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14]
+let arrStockageElement = [e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14]
 const bouttonLangue = document.querySelector('.container-langue')
 
 bouttonLangue.addEventListener('click', function(){
--- js/navigation.js
+++ js/navigation.js
@@ -1,107 +1,149 @@
-// import {lien1,lien2,lien3,lien4} from "./changementLangue.js"
-
 const sectionAccueil = document.getElementById('blanc')
 const sectionAPropos = document.getElementById('section-aPropos')
 const sectionProjet = document.getElementById('section-projet')
 const sectionContact = document.getElementById('section-contact')
-
-const bouttonL = document.querySelector('.container-langue')
-
-
 
 const l1 = document.querySelector('nav li:nth-child(1) a')
 const l2 = document.querySelector('nav li:nth-child(2) a')
 const l3 = document.querySelector('nav li:nth-child(3) a')
 const l4 = document.querySelector('nav li:nth-child(4) a')
 
-let arrL = [l1, l2, l3, l4]
-
 let scrollExplication = document.querySelector('footer h6')
+let burgerIcon = document.querySelector('.burger')
+let backgroundNav = document.querySelector('nav ul')
 
-// --------------- Wheel ---------------------------------------------------
-document.addEventListener('wheel', function (event) {
+let arrL = [l1, l2, l3, l4]
+let arrNav
+
+//---------- Mise a jour couleur onload ------------------------
+checkEverySectionPosition()
+miseAJourNav(checkPosition())
+miseAJourColor(checkPosition(), burgerIcon)
+miseAJourColor(checkPosition(), backgroundNav)
+// ------------ Navigation on scroll ------------------------------------------
+function checkDeltaY(parm) {                                                                       //----------- Vérifie si le scroll est positif ou negatif
+    if (parm.deltaY > 0) {
+        return true
+    } else {
+        return false
+    }
+}
+function checkEverySectionPosition() {                                                             //------------ Trouve la position du haut de chaque section par raport au haut du document
     let sectionAccueilTop = sectionAccueil.offsetTop
     let sectionAProposTop = sectionAPropos.offsetTop
     let sectionProjetTop = sectionProjet.offsetTop
     let sectionContactTop = sectionContact.offsetTop
-    let arrNav = [sectionAccueilTop, sectionAProposTop, sectionProjetTop, sectionContactTop]
-    
-    let delta = event.deltaY
-    let currentPos = document.documentElement.scrollTop
-    if (delta > 0) {
+
+    arrNav = [sectionAccueilTop, sectionAProposTop, sectionProjetTop, sectionContactTop]
+}
+function checkNextPosition(parm) {                                                                // ----------- retourne la position to scroll to en fonction du delta
+    if (parm == 1) {
         for (let i = 0; i < arrNav.length; i++) {
-            if (arrNav[i] > currentPos + 5) {
-                scrollTo(0, arrNav[i])
-                miseAJourNav(0)
-                miseAJourBoutton(i)
-                return
+            if (arrNav[i] > checkPosition()) {
+                return arrNav[i]
+            } 
+        }
+        return arrNav[3]    //-- pour ne pas revenir a la page d'acceuil lors du scroll
+    } else {
+        for (let i = arrNav.length; i >= 0; i--) {
+            if (arrNav[i] < checkPosition()) {
+                return arrNav[i]
             }
         }
     }
-    if (delta < 0) {
-        for (let i = arrNav.length - 1; i >= 0; i--) {
-            if (arrNav[i] < currentPos - 5) {
-                scrollTo(0, arrNav[i])
-                miseAJourNav(1)
-                miseAJourBoutton(i)
-                return
-            }
-        }
+}
+function checkPosition() {                                                                          //----------- trouve la position de la fenetre
+    let currentPosition = document.documentElement.scrollTop
+    return currentPosition
+}
+function scrollToPosition(parm) {
+    checkEverySectionPosition()
+    if (checkDeltaY(parm)) {
+        scrollTo(0, checkNextPosition(1))
+        miseAJourNav(checkNextPosition(1))
+        miseAJourColor(checkNextPosition(1), burgerIcon)
+        miseAJourColor(checkNextPosition(1), scrollExplication)
+        miseAJourColor(checkNextPosition(1), backgroundNav)
+        
+    } else {
+        scrollTo(0, checkNextPosition(0))
+        miseAJourNav(checkNextPosition(0))
+        miseAJourColor(checkNextPosition(0), burgerIcon)
+        miseAJourColor(checkNextPosition(0), scrollExplication)
+        miseAJourColor(checkNextPosition(0), backgroundNav)
     }
+}
+document.addEventListener('wheel', function (event) {
+    scrollToPosition(event)
+})
+// ---------------- Navigation on click -------------------------------------
+arrL.forEach((element)=>{
+    element.addEventListener('click', function(){
+        switch(element){
+            case l1: miseAJourColor(arrNav[0], backgroundNav); miseAJourColor(arrNav[0], scrollExplication); miseAJourColor(arrNav[0], burgerIcon); changeColor(0) ; break;
+            case l2: miseAJourColor(arrNav[1], backgroundNav); miseAJourColor(arrNav[1], scrollExplication); miseAJourColor(arrNav[1], burgerIcon); changeColor(1) ; break;
+            case l3: miseAJourColor(arrNav[2], backgroundNav); miseAJourColor(arrNav[2], scrollExplication); miseAJourColor(arrNav[2], burgerIcon); changeColor(2) ; break;
+            case l4: miseAJourColor(arrNav[3], backgroundNav); miseAJourColor(arrNav[3], scrollExplication); miseAJourColor(arrNav[3], burgerIcon); changeColor(3) ; break;
+        }
+    })
 })
 
-// --------------------Navigation changement de section ----------------
-
-
-function miseAJourNav(parm) {
-    let arrA = [sectionAccueil.getBoundingClientRect().top, sectionAPropos.getBoundingClientRect().top, sectionProjet.getBoundingClientRect().top, sectionContact.getBoundingClientRect().top]
-    for (let i = 0; i < arrA.length; i++) {
-        if (arrA[i] < 5 && arrA[i] > -5) {
-            if(parm == 0){
-                changeColor(i + 1)
-                changeColorF(i + 1)
-                return
-            } else {
-                changeColor(i - 1)
-                changeColorF(i - 1)
-                return
-            }
-        }
+//-----------------Mise à jour couleur de la nav ----------------------------
+function miseAJourNav(position) {
+    switch(position){
+        case arrNav[0]: changeColor(0) ; break;
+        case arrNav[1]: changeColor(1) ; break;
+        case arrNav[2]: changeColor(2) ; break;
+        case arrNav[3]: changeColor(3) ; break;
     }
 }
-function changeColor(parm) {
-    arrL.forEach((element) => {
-        if(parm == 0 || parm == 2){
-            element.style.color = "var(--col-White)"
-        } else {
-            element.style.color = "var(--col-black)"
-        }
-        element.style.fontSize = "0.8em"
-    })
-    arrL[parm].style.color = "rgb(28, 67, 238)"
-    arrL[parm].style.fontSize = "1.3em"
-    arrL[parm].style.transition = "all 0.5s"
-}
-function changeColorF(parm) {
+function changeColor (parm){
+
     if(parm == 0 || parm == 2){
-        scrollExplication.style.color = "var(--col-White)"
-        scrollExplication.style.transition = "color 0.5s"
+        arrL.forEach((element)=>{
+            element.classList.remove('navColorClasseSpecial')
+            element.classList.remove('navColorClasseWhite')
+            element.classList.remove('navColorClasseBlack')
+            element.classList.add('navColorClasseWhite')
+        })
     } else {
-        scrollExplication.style.color = "var(--col-black)"
-        scrollExplication.style.transition = "color 0.5s"
+        arrL.forEach((element)=>{
+            element.classList.remove('navColorClasseSpecial')
+            element.classList.remove('navColorClasseWhite')
+            element.classList.remove('navColorClasseBlack')
+            element.classList.add('navColorClasseBlack')
+        })
+    }
+    arrL[parm].classList.remove('navColorClasseWhite')
+    arrL[parm].classList.remove('navColorClasseBlack')
+    arrL[parm].classList.add('navColorClasseSpecial')
+}
+//---------Mise à jour color burger-----------------------------------------
+function miseAJourColor(position, element){
+    switch(position){
+        case arrNav[0]: elementChangeColor(0, element)  ; break;
+        case arrNav[1]: elementChangeColor(1, element)  ; break;
+        case arrNav[2]: elementChangeColor(0, element)  ; break;
+        case arrNav[3]: elementChangeColor(1, element)  ; break;
     }
 }
-
-// ---------------Mise a jour du boutton langue --------------------------
-
-function miseAJourBoutton(parm) {
-    if(parm !== 0){
-        bouttonL.style.opacity = 0
-        bouttonL.style.transition = "all 0.5s"
-        bouttonL.style.cursor = "default"
+function elementChangeColor (parm, element){
+    
+    if(parm == 0){
+        if(element == backgroundNav){
+            element.classList.remove('ulBackgroundWhite')
+            element.classList.add('ulBackgroundBlack')
+        } else {
+            element.classList.remove('elementColorClasseBlack')
+            element.classList.add('elementColorClasseWhite')
+        }
     } else {
-        bouttonL.style.opacity = 1
-        bouttonL.style.transition = "all 0.5s"
-        bouttonL.style.cursor = "pointer"
+        if(element == backgroundNav){
+            element.classList.remove('ulBackgroundBlack')
+            element.classList.add('ulBackgroundWhite')
+        } else {
+            element.classList.remove('elementColorClasseWhite')
+            element.classList.add('elementColorClasseBlack')
+        }
     }
 }
